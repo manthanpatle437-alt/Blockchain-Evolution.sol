43dbe3a4379c3e1cec8171613dad169d7dc0ec3a
manthanpatle437-alt <manthanpatle437@gmail.com>
2025-11-28 11:21:35 +0530

    Update Blockchain Evolution.sol

--- Contract/Blockchain Evolution.sol
+++ Contract/Blockchain Evolution.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 /**
  * @title Blockchain Evolution
- * @dev A simple smart contract to demonstrate blockchain data storage and interaction.
+ * @dev A simple smart contract to demonstrate blockchain  data storage and interaction.
  */
 contract BlockchainEvolution {
 
